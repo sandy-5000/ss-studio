ce1a9d44f8ada9ab444a9f37f82d864f68d754f5
sandy-5000 <sandeepkumarbhaviri@gmail.com>
2024-10-06 14:28:18 +0530

    made template

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import '@/app/globals.css'
+
+const Page = () => {
+  return (
+    <div className="h-screen v-screen a-center">
+      <p>About!</p>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/home/page.js
@@ -0,0 +1,11 @@
+import '@/app/globals.css'
+
+const Page = () => {
+  return (
+    <div className="h-screen v-screen a-center">
+      <p>Home!</p>
+    </div>
+  )
+}
+
+export default Page
